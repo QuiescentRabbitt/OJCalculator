e102fa145017fadfcfdec067ac4e80edda406811
QuiescentRabbitt <29581638+QuiescentRabbitt@users.noreply.github.com>
2018-12-22 08:11:12 -0500

    Update OrangeJuiceMain.java

--- OJCalculator/src/OrangeJuiceMain.java
+++ OJCalculator/src/OrangeJuiceMain.java
@@ -3,6 +3,8 @@ public class OrangeJuiceMain {
 
 	
 	public static void main (String[] args) {
+		//@QuiescentRabbitt
+		//OJCalculator 8/14/18
 		
 		JFrame gridFrame = new JFrame ("OJ");
 		gridFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
